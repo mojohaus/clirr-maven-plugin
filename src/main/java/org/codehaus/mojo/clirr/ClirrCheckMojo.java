e30b2c5edb4e584d32092c8d65354c8226a09fa3
Brett Porter <brett@apache.org>
2006-02-12 16:21:30 +0000

    refactor and add check mojo
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/mojo-sandbox/clirr-maven-plugin@1473 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/clirr/ClirrCheckMojo.java
+++ src/main/java/org/codehaus/mojo/clirr/ClirrCheckMojo.java
@@ -16,9 +16,19 @@ package org.codehaus.mojo.clirr;
  * limitations under the License.
  */
 
+import net.sf.clirr.core.ApiDifference;
+import net.sf.clirr.core.Severity;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.util.Iterator;
 
 /**
  * Perform a violation check against the last checkstyle run to see if there are any violations.
@@ -26,39 +36,156 @@ import org.apache.maven.plugin.MojoFailureException;
  * @author <a href="mailto:brett@apache.org">Brett Porter</a>
  * @goal check
  * @phase verify
- * @todo write
+ * @execute phase="compile"
+ * @requiresDependencyResolution compile
+ * @todo i18n
  */
 public class ClirrCheckMojo
     extends AbstractMojo
 {
     /**
-     * @parameter default-value="false"
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
      */
-    private boolean failOnSrcWarning;
+    private MavenProject project;
 
     /**
-     * @parameter default-value="false"
+     * @component
      */
-    private boolean failOnBinWarning;
+    private ArtifactResolver resolver;
 
     /**
-     * @parameter default-value="true"
+     * @component
      */
-    private boolean failOnSrcError;
+    private ArtifactFactory factory;
 
     /**
-     * @parameter default-value="true"
+     * @parameter default-value="${localRepository}"
+     * @required
+     * @readonly
      */
-    private boolean failOnBinError;
+    private ArtifactRepository localRepository;
 
     /**
-     * @parameter expression="${previousVersion}"
+     * @component
      */
-    private String previousVersion;
+    private ArtifactMetadataSource metadataSource;
+
+    /**
+     * The classes of this project to compare the last release against.
+     *
+     * @parameter default-value="${project.build.outputDirectory}
+     */
+    private File classesDirectory;
+
+    /**
+     * Version to compare the current code against.
+     *
+     * @parameter expression="${comparisonVersion}" default-value="(,${project.version})"
+     */
+    private String comparisonVersion;
+
+    /**
+     * A text output file to render to. If omitted, no output is rendered to a text file.
+     *
+     * @parameter expression="${textOutputFile}"
+     */
+    private File textOutputFile;
+
+    /**
+     * An XML file to render to. If omitted, no output is rendered to an XML file.
+     *
+     * @parameter expression="${xmlOutputFile}"
+     */
+    private File xmlOutputFile;
+
+    /**
+     * A list of classes to include. Anything not included is excluded. If omitted, all are assumed to be included.
+     * Values are specified in path pattern notation, e.g. <code>org/codehaus/mojo/**</code>.
+     *
+     * @parameter
+     */
+    private String[] includes;
+
+    /**
+     * A list of classes to exclude. These classes are excluded from the list of classes that are included.
+     * Values are specified in path pattern notation, e.g. <code>org/codehaus/mojo/**</code>.
+     *
+     * @parameter
+     */
+    private String[] excludes;
+
+    /**
+     * Whether to log the results to the console or not.
+     *
+     * @parameter expression="${logResults}" default-value="false"
+     */
+    private boolean logResults;
+
+    /**
+     * Whether to fail on errors.
+     *
+     * @parameter expression="${failOnError}" default-value="true"
+     */
+    private boolean failOnError;
+
+    /**
+     * Whether to fail on warnings.
+     *
+     * @parameter expression="${failOnWarning}" default-value="false"
+     */
+    private boolean failOnWarning;
 
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
-        // TODO: execute
+        ClirrExecutor executor = new ClirrExecutor();
+        if ( logResults )
+        {
+            executor.setLog( getLog() );
+        }
+        executor.setTextOutputFile( textOutputFile );
+        executor.setXmlOutputFile( xmlOutputFile );
+        executor.setIncludes( includes );
+        executor.setExcludes( excludes );
+        executor.setComparisonVersion( comparisonVersion );
+        executor.setClassesDirectory( classesDirectory );
+        executor.execute( project, resolver, metadataSource, localRepository, factory, getLog() );
+
+        ClirrDiffListener listener = executor.getListener();
+        int errorCount = listener.getSeverityCount( Severity.ERROR );
+        if ( failOnError && errorCount > 0 )
+        {
+            log( listener, Severity.ERROR );
+            throw new MojoFailureException( "There were " + errorCount + " errors" );
+        }
+
+        int warningCount = listener.getSeverityCount( Severity.WARNING );
+        if ( failOnWarning && errorCount > 0 )
+        {
+            log( listener, Severity.WARNING );
+            throw new MojoFailureException( "There were " + warningCount + " errors" );
+        }
+
+        int infoCount = listener.getSeverityCount( Severity.INFO );
+        getLog().info( "Succeeded with " + errorCount + " errors; " + warningCount + " warnings; and " + infoCount +
+            " other changes" );
+    }
+
+    private void log( ClirrDiffListener listener, Severity severity )
+    {
+        if ( !logResults )
+        {
+            LogDiffListener l = new LogDiffListener( getLog() );
+            for ( Iterator i = listener.getApiDifferences().iterator(); i.hasNext(); )
+            {
+                ApiDifference difference = (ApiDifference) i.next();
+                if ( difference.getMaximumSeverity().equals( severity ) )
+                {
+                    l.reportDiff( difference );
+                }
+            }
+        }
     }
 }

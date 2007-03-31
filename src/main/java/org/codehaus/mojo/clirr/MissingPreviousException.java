7f50aeb64c30004de1b2dab26d11671327b97f24
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2007-03-31 15:28:11 +0000

    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/clirr-maven-plugin@3808 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/clirr/MissingPreviousException.java
@@ -0,0 +1,15 @@
+package org.codehaus.mojo.clirr;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+
+/**
+ * This exception is thrown, if no previous version of the current artifact was found.
+ */
+class MissingPreviousException extends MojoExecutionException {
+    private static final long serialVersionUID = -5160106292241626179L;
+
+    MissingPreviousException(String pMessage, Throwable pCause) {
+        super(pMessage, pCause);
+    }
+}

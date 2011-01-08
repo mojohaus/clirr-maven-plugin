bc55dfbf0e20b35d07d17ee9857242f2c39ab50f
Olivier Lamy <olamy@apache.org>
2011-01-08 00:04:24 +0000

    really fix [MCLIRR-29]  add a check-no-fork goal.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/clirr-maven-plugin@13369 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/clirr/ClirrCheckMojo.java
+++ src/main/java/org/codehaus/mojo/clirr/ClirrCheckMojo.java
@@ -16,14 +16,6 @@ package org.codehaus.mojo.clirr;
  * limitations under the License.
  */
 
-import net.sf.clirr.core.ApiDifference;
-import net.sf.clirr.core.Severity;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.codehaus.plexus.i18n.I18N;
-
-import java.util.Iterator;
-import java.util.Locale;
 
 /**
  * Check for compatibility with previous version.
@@ -31,131 +23,10 @@ import java.util.Locale;
  * @author <a href="mailto:brett@apache.org">Brett Porter</a>
  * @goal check
  * @phase verify
+ * @execute phase="compile"
  */
 public class ClirrCheckMojo
-    extends AbstractClirrMojo
+    extends AbstractClirrCheckMojo
 {
-    /**
-     * Whether to fail on errors.
-     *
-     * @parameter expression="${failOnError}" default-value="true"
-     */
-    private boolean failOnError;
-
-    /**
-     * Whether to fail on warnings.
-     *
-     * @parameter expression="${failOnWarning}" default-value="false"
-     */
-    private boolean failOnWarning;
-
-    /**
-     * Whether to fail on info.
-     *
-     * @parameter expression="${failOnInfo}" default-value="false"
-     */
-    private boolean failOnInfo;
-
-    /**
-     * @component
-     */
-    private I18N i18n;
-
-    protected void doExecute()
-        throws MojoExecutionException, MojoFailureException
-    {
-        if ( !canGenerate() )
-        {
-            return;
-        }
-        Severity minSeverity = convertSeverity( this.minSeverity );
-        
-        
-        ClirrDiffListener listener;
-        try
-        {
-            listener = executeClirr(minSeverity);
-        }
-        catch ( MissingPreviousException e )
-        {
-            getLog().debug( e );
-            getLog().info( "No previous version was found. Use 'comparisonArtifacts'"
-                    + " for explicit configuration if you think this is wrong." );
-            return;
-        }
-
-        Locale locale = Locale.getDefault();
-
-        int errorCount = listener.getSeverityCount( Severity.ERROR );
-        if ( failOnError && errorCount > 0 )
-        {
-            log( listener, Severity.ERROR );
-            String message;
-            if ( errorCount > 1 )
-            {
-                String[] args = new String[]{String.valueOf( errorCount )};
-                message = i18n.format( "clirr-report", locale, "check.clirr.failure.errors", args );
-            }
-            else
-            {
-                message = i18n.getString( "clirr-report", locale, "check.clirr.failure.error" );
-            }
-            throw new MojoFailureException( message );
-        }
-
-        int warningCount = listener.getSeverityCount( Severity.WARNING );
-        if ( failOnWarning && warningCount > 0 )
-        {
-            log( listener, Severity.WARNING );
-            String message;
-            if ( warningCount > 1 )
-            {
-                String[] args = new String[]{String.valueOf( warningCount )};
-                message = i18n.format( "clirr-report", locale, "check.clirr.failure.warnings", args );
-            }
-            else
-            {
-                message = i18n.getString( "clirr-report", locale, "check.clirr.failure.warning" );
-            }
-            throw new MojoFailureException( message );
-        }
-
-        int infoCount = listener.getSeverityCount( Severity.INFO );
-        if ( failOnInfo && infoCount > 0)
-        {
-            log( listener, Severity.INFO );
-            String message;
-            if ( infoCount > 1 )
-            {
-                String[] args = new String[]{String.valueOf( infoCount )};
-                message = i18n.format( "clirr-report", locale, "check.clirr.failure.infos", args );
-            }
-            else
-            {
-                message = i18n.getString( "clirr-report", locale, "check.clirr.failure.info" );
-            }
-            throw new MojoFailureException( message );
-        }
-
-        String[] args =
-            new String[]{String.valueOf( errorCount ), String.valueOf( warningCount ), String.valueOf( infoCount )};
-        getLog().info( i18n.format( "clirr-report", locale, "check.clirr.success", args ) );
-    }
-
-    private void log( ClirrDiffListener listener, Severity severity )
-    {
-        if ( !logResults )
-        {
-            LogDiffListener l = new LogDiffListener( getLog() );
-            for ( Iterator i = listener.getApiDifferences().iterator(); i.hasNext(); )
-            {
-                ApiDifference difference = (ApiDifference) i.next();
-                if ( difference.getMaximumSeverity().equals( severity ) )
-                {
-                    l.reportDiff( difference );
-                }
-            }
-        }
-    }
-
+    // no op
 }

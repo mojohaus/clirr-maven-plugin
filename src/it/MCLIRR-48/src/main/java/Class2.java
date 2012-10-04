1254a473b9c936e27b2e1bd7e9ef16a7987d7d69
Robert Scholte <rfscholte@apache.org>
2012-10-04 20:50:37 +0000

    Rearrange tests for MCLIRR-48
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/clirr-maven-plugin@17627 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/MCLIRR-48/src/main/java/Class2.java
@@ -0,0 +1,5 @@
+import java.io.Serializable;
+
+public class Class2 extends Base implements Serializable {
+    public float publicField;    
+}
---
+++ src/it/setup-MCLIRR-48/src/main/java/Class2.java
@@ -0,0 +1,3 @@
+public class Class2 {
+    public int publicField;    
+}

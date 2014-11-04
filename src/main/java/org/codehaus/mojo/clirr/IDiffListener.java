6eb727f301b7520bf997d1a57ce7f374963cad75
Olivier Lamy <olamy@apache.org>
2014-11-04 11:30:13 +0000

    [MCLIRR-63] Include API changelog in Clirr report Submitted by Jean-Noel Rouvignac
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/clirr-maven-plugin@20153 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/clirr/IDiffListener.java
@@ -0,0 +1,31 @@
+package org.codehaus.mojo.clirr;
+
+/*
+ * Copyright 2014 ForgeRock AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import net.sf.clirr.core.ApiDifference;
+import net.sf.clirr.core.DiffListener;
+
+/**
+ * DiffListener that can listen for ignored differences.
+ */
+public interface IDiffListener
+    extends DiffListener
+{
+
+    void reportIgnoredDiff( ApiDifference ignoredDiff, Difference reason );
+
+}

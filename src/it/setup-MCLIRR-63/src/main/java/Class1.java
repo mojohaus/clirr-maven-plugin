68bb6714234be5c9177cc6b15d1a64b66e03b66b
Olivier Lamy <olamy@apache.org>
2014-11-14 05:43:43 +0000

    add some it for MCLIRR-63
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/clirr-maven-plugin@20199 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/setup-MCLIRR-63/src/main/java/Class1.java
@@ -0,0 +1,42 @@
+import java.io.Serializable;
+
+public class Class1 extends Base implements Serializable {
+
+    public int publicField;
+
+    class SubClass {
+
+    }
+
+    public void voidMethod() {
+
+    }
+
+    public void paramsMethod1(int p1, String p2) {
+
+    }
+
+    public void paramsMethod2(int p1, String p2) {
+
+    }
+
+    public int returnMethod() {
+        return 0;
+    }
+
+    public Class1 fluent1() {
+        return null;
+    }
+
+    public String fluent2() {
+        return null;
+    }
+
+    public SubClass returnSubClass1() {
+        return null;
+    }
+
+    public String returnSubClass2() {
+        return null;
+    }
+}
